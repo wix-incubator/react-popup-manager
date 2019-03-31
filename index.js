d420eff850ec3a95209e98f0c3460c57ade03eff
varzager <varzager@gmail.com>
2019-03-31 21:12:28 +0300

    #EE-17584| fixing index.js of root

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require('./dist/src/index.js');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var PopupProvider_1 = require("./dist/src/PopupProvider");
+exports.PopupProvider = PopupProvider_1.PopupProvider;
+var popupManager_1 = require("./dist/src/popupManager");
+exports.PopupManager = popupManager_1.PopupManager;
+var withPopups_1 = require("./dist/src/withPopups");
+exports.withPopups = withPopups_1.withPopups;
+//# sourceMappingURL=index.js.map
