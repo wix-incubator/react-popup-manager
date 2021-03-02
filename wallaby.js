e10f75dcd0f7ba6332df148b0094dbde2f6abd36
Dean Shub <deanshub@gmail.com>
2021-03-02 08:27:32 +0000

    Nissix "scope-packages" plugin

--- wallaby.js
+++ wallaby.js
@@ -1,5 +1,5 @@
 module.exports = function(wallaby) {
-  return Object.assign({}, require('yoshi/config/wallaby-jest')(wallaby), {
+  return Object.assign({}, require('@wix/yoshi/config/wallaby-jest')(wallaby), {
     // set to undefined to let Wallaby decide the number of processes based on the system's capacity
     workers: undefined,
   });
