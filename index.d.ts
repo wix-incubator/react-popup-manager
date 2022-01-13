14da2b6b67721f5e9f46a1a0026dd69cb3fae7ed
guyw <guyw@wix.com>
2022-01-13 19:22:55 +0200

    fix(react-popup-manager) - export `usePopupManager`

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-import { PopupProvider } from './dist/src/PopupProvider';
-import { PopupManager } from './dist/src/popupManager';
-import { withPopups } from './dist/src/withPopups';
-import { WithPopupsProps, PopupProps } from './dist/src/popupsDef';
-export { PopupProps, PopupProvider, PopupManager, withPopups, WithPopupsProps };
--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var PopupProvider_1 = require("./dist/src/PopupProvider");
-exports.PopupProvider = PopupProvider_1.PopupProvider;
-var popupManager_1 = require("./dist/src/popupManager");
-exports.PopupManager = popupManager_1.PopupManager;
-var withPopups_1 = require("./dist/src/withPopups");
-exports.withPopups = withPopups_1.withPopups;
-//# sourceMappingURL=index.js.map
